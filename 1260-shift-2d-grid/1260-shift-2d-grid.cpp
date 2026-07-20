b2fd225446d31fe03100254e0099e98f2e71edb5
Vaibhav Chaudhary <99083629+Heromontage@users.noreply.github.com>
2026-07-20 16:43:32 +0530

    Time: 21 ms (7.07%), Space: 18.8 MB (20.65%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -20,8 +20,6 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
-        int totalElements = m * n;
-        k = k % totalElements;
         if (k == 0) return grid;
         vector<vector<int>> nextGrid = grid;
         shiftRecursive(grid, nextGrid, m, n, k);
