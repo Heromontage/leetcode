ec1073f9f9c1e9f5a5b0e9dbecbae72711e0143a
Vaibhav Chaudhary <99083629+Heromontage@users.noreply.github.com>
2026-09-11 19:02:38 +0530

    Time: 28 ms (17.09%), Space: 37.4 MB (14.41%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    set<vector<int>> st;
+
+    void help(const vector<int>& digits, vector<bool>& used, vector<int>& combin, vector<vector<int>>& ans) {
+        if (combin.size() == 3) {
+            if (combin[0] != 0 && combin[2] % 2 == 0 && st.insert(combin).second)
+                ans.push_back(combin);
+            return;
+        }
+
+        for (int i = 0; i < digits.size(); ++i) {
+            if (used[i]) continue;
+            used[i] = true;
+            combin.push_back(digits[i]);
+            help(digits, used, combin, ans);
+            combin.pop_back();
+            used[i] = false;
+        }
+    }
+    int totalNumbers(vector<int>& digits) {
+        vector<int>combin;
+        vector<vector<int>>ans;
+        vector<bool>use(digits.size(),false);
+        help(digits,use,combin,ans);
+        return ans.size();
+    }
+};
